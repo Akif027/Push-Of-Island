2878cc8694d0aa83bce2d4a4cf718b2d1cf7117b
akif shaikh <66055292+Akif027@users.noreply.github.com>
2025-01-12 23:09:00 +0530

    fix everything based on tasks

--- Assets/InGame/Scripts/TokenManager.cs
+++ Assets/InGame/Scripts/TokenManager.cs
@@ -91,6 +91,7 @@ public class TokenManager : MonoBehaviour
             MapScroll.Instance.EnableScroll();
             selectedToken.OnTokenDeselected();
             selectedToken = null; // Clear the selected token reference
+            if (selectedToken.owner != GameManager.Instance.GetCurrentPlayer()) return;
             UIManager.Instance.OpenPlayLowerPanel();
             UIManager.Instance.ClosePlayAttackLowerPanel();
             Debug.Log("Token deselected.");
@@ -105,7 +106,7 @@ public class TokenManager : MonoBehaviour
         }
         MapScroll.Instance.DisableScroll();
         selectedToken = token;
-
+        if (selectedToken.owner != GameManager.Instance.GetCurrentPlayer()) return;
         UIManager.Instance.ClosePlayLowerPanel();
         UIManager.Instance.OpenPlayAttackLowerPanel();
         selectedToken.OnTokenSelected();
